77100bc4b8503ac1de70cac9a372a6aa13e5aa47
Joshua Chen <sidachen2003@gmail.com>
2022-12-11 01:15:30 -0500

    Day 11

--- Day11/main.hs
+++ Day11/main.hs
@@ -2,17 +2,19 @@
 
 import Data.Char
 import Data.List
-import Data.Ord
 import Data.Map qualified as Map
+import Data.Ord
 import Data.Text qualified as T
 import Utils
 
 main = do
   input <- getInput
   let monkeys = Map.fromList $ map parseMonkey $ T.splitOn (T.pack "\n\n") input
-  let monkeys' = foldr (const doRound) monkeys [1 .. 20]
-  let times = sortOn Down $ map (\m -> inspectTimes m) $ Map.elems monkeys'
-  print $ (times !! 0 * times !! 1)
+  let monkeys' = passAround 20 3 monkeys
+  print $ monkeyBusiness monkeys'
+
+  let monkeys' = passAround 10000 1 monkeys
+  print $ monkeyBusiness monkeys'
 
 data Monkey = Monkey
   { items :: [Int],
@@ -24,7 +26,8 @@ data Monkey = Monkey
   }
 
 instance Show Monkey where
-  show (Monkey {items, operation, test, target1, target2, inspectTimes}) = "Items " ++ show items ++ " operation 2 -> " ++ show (operation 2) ++ " test " ++ show test ++ " to " ++ show target1 ++ " or " ++ show target2 ++ " inspectTimes " ++ show inspectTimes
+  show (Monkey {items, operation, test, target1, target2, inspectTimes}) =
+    "Items " ++ show items ++ " operation 2 -> " ++ show (operation 2) ++ " test " ++ show test ++ " to " ++ show target1 ++ " or " ++ show target2 ++ " inspectTimes " ++ show inspectTimes
 
 parseMonkey :: T.Text -> (Int, Monkey)
 parseMonkey text =
@@ -32,31 +35,41 @@ parseMonkey text =
   where
     [l1, l2, l3, l4, l5, l6] = T.lines text
     number = digitToInt (T.unpack l1 !! 7)
-    items = reverse $ map readT $ T.splitOn (T.pack ", ") $ T.drop (length "  Starting items: ") l2
-    operation = case T.unpack $ T.drop (length "  Operation: new = old ") l3 of
+    items = reverse $ map readT $ T.splitOn (T.pack ", ") $ dropPrefix "  Starting items: " l2
+    operation = case T.unpack $ dropPrefix "  Operation: new = old " l3 of
       "* old" -> (^ 2)
       '*' : num -> (* (read $ tail num))
       '+' : num -> (+ (read $ tail num))
-    test = readT $ T.drop (length "  Test: divisible by ") l4
-    target1 = readT $ T.drop (length "    If true: throw to monkey ") l5
-    target2 = readT $ T.drop (length "    If false: throw to monkey ") l6
+    test = readAfter "  Test: divisible by " l4
+    target1 = readAfter "    If true: throw to monkey " l5
+    target2 = readAfter "    If false: throw to monkey " l6
+    dropPrefix = T.drop . length
+    readAfter prefix = readT . dropPrefix prefix
 
-monkeyInspect :: Monkey -> Int -> (Int, Int)
-monkeyInspect (Monkey {items, operation, test, target1, target2}) item = case item' `mod` test of
+monkeyInspect :: Monkey -> (Int, Int) -> Int -> (Int, Int)
+monkeyInspect (Monkey {items, operation, test, target1, target2}) (relief, divisor) item = case item' `mod` test of
   0 -> (target1, item')
   _ -> (target2, item')
   where
-    item' = operation item `div` 3
+    item' = operation item `div` relief `mod` divisor
 
-monkeyDoRound :: Map.Map Int Monkey -> Int -> Map.Map Int Monkey
-monkeyDoRound monkeys k = monkeys'
+monkeyDoRound :: (Int, Int) -> Map.Map Int Monkey -> Int -> Map.Map Int Monkey
+monkeyDoRound reducer monkeys k = updateSelf $ foldr throwToTarget monkeys targets
   where
     monkey = monkeys ! k
-    targets = map (monkeyInspect monkey) $ items monkey
-    monkeys' =
-      Map.adjust (\m -> m {items = [], inspectTimes = inspectTimes m + length targets}) k $
-        foldr throwToTarget monkeys targets
+    targets = map (monkeyInspect monkey reducer) $ items monkey
+    updateSelf = Map.adjust (\m -> m {items = [], inspectTimes = inspectTimes m + length targets}) k
     throwToTarget (target, item) = Map.adjust (\m -> m {items = item : items m}) target
 
-doRound :: Map.Map Int Monkey -> Map.Map Int Monkey
-doRound monkeys = foldl' monkeyDoRound monkeys (Map.keys monkeys)
+doRound :: (Int, Int) -> Map.Map Int Monkey -> Map.Map Int Monkey
+doRound reducer monkeys = foldl' (monkeyDoRound reducer) monkeys (Map.keys monkeys)
+
+passAround :: Int -> Int -> Map.Map Int Monkey -> Map.Map Int Monkey
+passAround rounds relief monkeys = foldr (const $ doRound (relief, divisor)) monkeys [1 .. rounds]
+  where
+    divisor = Map.foldr (lcm . test) 1 monkeys
+
+monkeyBusiness :: Map.Map Int Monkey -> Int
+monkeyBusiness monkeys = fst * snd
+  where
+    fst : snd : rest = sortOn Down $ map inspectTimes $ Map.elems monkeys
